3294d2e4ddb8c2ad94ee2c77460ee1f871be944c
muratarslan35 <102037882+muratarslan35@users.noreply.github.com>
2026-01-22 00:20:18 +0300

    Update app.py

--- app.py
+++ app.py
@@ -94,7 +94,7 @@ def send_telegram_message(text: str):
 # ======================================================
 def send_startup_message():
     send_telegram_message(
-        "🟢 <b>TUKAŞA MEYDAN OKUYAN BOT😎 - TEST EDİLİYOR⏳️</b>\n"
+        "🟢 <b>HİSSE TARAMASI BAŞLADI⏳️</b>\n"
         f"🕒 {now_tr().strftime('%H:%M:%S')} | {now_tr().strftime('%d.%m.%Y')}"
     )
 
